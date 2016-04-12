e01b8acfaebd17167dc3b96de77f6964bb8f78a4
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-04-11 21:33:39 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews
   has_many :product, through: :reviews
+
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411213130) do
+ActiveRecord::Schema.define(version: 20160411225807) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160411213130) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
